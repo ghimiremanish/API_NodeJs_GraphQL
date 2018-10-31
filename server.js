270e71bcf7a25fff36cce4d6677ecc5a99f9e0e5
ghimiremanish <manishghimire153200@gmail.com>
2018-10-31 09:07:44 +0545

    second

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-var { graphql, buildSchema } = require('graphql');
+// var { graphql, buildSchema } = require('graphql');
+
+// // Construct a schema, using GraphQL schema language
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
+
+// // The root provides a resolver function for each API endpoint
+// var root = {
+//   hello: () => {
+//     return 'Hello world!';
+//   },
+// };
+
+// // Run the GraphQL query '{ hello }' and print out the response
+// graphql(schema, '{ hello }', root).then((response) => {
+//   console.log(response);
+// });
+
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -14,8 +37,11 @@ var root = {
   },
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
-
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
